ffa929e44ce4ec3e03131afdc34258f5f1330807
glennall <glennlenormand@gmail.com>
2025-01-05 08:37:18 -0600

    microservice archi

---
+++ apps/orders/src/orders.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { OrdersController } from './orders.controller';
+import { OrdersService } from './orders.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '@app/common';
+import * as Joi from 'joi';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
+    DatabaseModule,
+  ],
+  controllers: [OrdersController],
+  providers: [OrdersService],
+})
+export class OrdersModule { }
